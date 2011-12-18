791f91ee1d686f1790b48bb48baabf8e84930c29
Ivan K <divout@gmail.com>
2011-12-18 15:24:38 +0600

    moved parse_date_time and replace_with methods to Array class

--- lib/previews/array.rb
+++ lib/previews/array.rb
@@ -7,13 +7,30 @@ class Array
                 :system_less => system_less
   end
 
+  # By default reviews sorted by usefulness. We can reverse it to sort by ascending
+  # For others fields we sorting by comparing each element of Array
   def sort_with!(field, direction)
     self          if 'usefulness' == field && 'desc' == direction
     self.reverse! if 'usefulness' == field && 'asc'  == direction
     if 'asc' == direction
       self.sort! { |review1, review2| review1[field] <=> review2[field]}
-    else
+    elsif 'desc' == direction
       self.sort! { |review1, review2| review2[field] <=> review1[field]}
+    end
+  end
+
+  # Parses all 'date_created' fields to DateTime object
+  def parse_date_time
+    self.each do |review|
+      review['date_created'] = DateTime.parse(review['date_created'])
+    end
+  end
+
+  # Replace each review's rating into some symbols
+  # Usage: array.replace_rating_with("★", "☆")
+  def replace_rating_with(up, empty)
+    self.each do |review|
+      review['rating'] = up * review['rating'] + empty * (5 - review['rating'])
     end
   end
 end
